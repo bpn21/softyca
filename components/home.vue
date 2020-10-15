025082c57e6184430a81e5238595e7c0c77907f6
bpn21 <bpngaire@gmail.com>
2020-10-15 11:01:20 +0545

    api hit

---
+++ components/home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Get data from api</h1>
+    </div>
+</template>
---
+++ plugins/axios.js
@@ -0,0 +1,19 @@
+export default function ({ $axios, redirect }) {
+    // $axios.setBaseURL(process.env.AXIOS_BASE_URL)
+    // $axios.setBaseURL('process.env.AXIOS_BASE_URL')
+    $axios.setBaseURL('http://localhost:8000/')
+
+    $axios.onRequest((config) => {
+      console.log('Making request to ' + config.url)
+    })
+  
+    $axios.onError((error) => {
+      const code = parseInt(error.response && error.response.status)
+      if (code === 500) {
+        redirect('/500')
+      }
+      if (code === 403) {
+        redirect('/account/login')
+      }
+    })
+  }
