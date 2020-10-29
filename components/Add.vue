2361bd9395831ca96db01bd1cce4773290c492a7
bpn21 <bpngaire@gmail.com>
2020-10-29 16:33:23 +0545

    worked on vue-modal

--- components/Add.vue
+++ components/Add.vue
@@ -56,6 +56,7 @@ export default {
   name: 'Add',
   components: {
      'dropdown': dropdown,
+ 
   },
     data(){
         return{
--- pages/list-product.vue
+++ pages/list-product.vue
@@ -3,76 +3,72 @@
   <div class="container bg-teal-900" >
       <div class="flex flex-wrap ">
 
-          <h1 class="mx-6 font-sans block shadow-lg text-5xl font-extrabold text-white rounded-lg border p-1 bg-teal-700 my-5" >These are the list of Products in Database.</h1> 
-          
-          <div class=" py-8 px-3  my-5 mx-6 font-sans shadow-2xl w-3/12 bg-white block shadow-xl text-black rounded-lg border-double bg-teal-800 text-white" v-for="(product,id) in list" :key="`${id}-ec`">
-            <div class=" my-4 p-3 rounded-lg shadow-lg border bg-white capitalize text-black ">
-              Detail of {{product.name}}
-            </div>
-            <div class="uppercase bg-green-700 text-white-500 rounded-lg ">
-            Category: {{product.category_name ||"not avaliable"}},<br>
-            </div>
-            Name:  {{product.name||"not avaliable"}},<br>
-            Size : {{product.size||"not avaliable"}}, <br>
-            Color :{{product.color||"not avaliable"}},<br>
-            Brand :{{product.brand||"not avaliable"}},<br>
-            Id: {{product.id ||"not avaliable"}}
-            <div class="flex">
-
-              <button @click="toggleEditModal(product)">
-                <svg class="stroke-2  w-10 h-6 hover:bg-teal-900 rounded-lg"  fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/></svg>
-              </button>
-
-
-              <button @click="toggleDeleteModal(product)">
-                <svg class="w-10 h-6 text-teal-100 hover:bg-red-700 rounded-lg" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-1 12a2 2 0 01-2 2H8a2 2 0 01-2-2L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/></svg>
-              </button>
-            </div>
-
+        <h1 class="mx-6 font-sans block shadow-lg text-5xl font-extrabold text-white rounded-lg border p-1 bg-teal-700 my-5" >These are the list of Products in Database.</h1> 
+        
+        <div class=" py-8 px-3  my-5 mx-6 font-sans shadow-2xl w-3/12 bg-white block shadow-xl text-black rounded-lg border-double bg-teal-800 text-white" v-for="(product,id) in list" :key="`${id}-ec`">
+          <div class=" my-4 p-3 rounded-lg shadow-lg border bg-white capitalize text-black ">
+            Detail of {{product.name}}
           </div>
-
-          <button class="fixed inset-x-0 " @click="toggleAddModal()">
-            <svg class=" w-24 h-24 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/></svg> 
-          </button> 
-
-          <div v-if=showDeleteModal> 
-            <ConformDelete
-            :id=id
-            :data=data
-            @delete=ConformDelete
-            />
+          <div class="uppercase bg-green-700 text-white-500 rounded-lg ">
+          Category: {{product.category_name ||"not avaliable"}},<br>
           </div>
+          Name:  {{product.name||"not avaliable"}},<br>
+          Size : {{product.size||"not avaliable"}}, <br>
+          Color :{{product.color||"not avaliable"}},<br>
+          Brand :{{product.brand||"not avaliable"}},<br>
+          Id: {{product.id ||"not avaliable"}}
+          <div class="flex">
+            <button @click="toggleEditModal(product)">
+              <svg class="stroke-2  w-10 h-6 hover:bg-teal-900 rounded-lg"  fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/></svg>
+            </button>
 
-          <div v-if=showEditModal>
-            <Edit
-            :categoryList=categoryList
-            :id=id
-            :data=data
-            @edit=Edit
-            />
+            <button @click="toggleDeleteModal(product)">
+              <svg class="w-10 h-6 text-teal-100 hover:bg-red-700 rounded-lg" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-1 12a2 2 0 01-2 2H8a2 2 0 01-2-2L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/></svg>
+            </button>
           </div>
+        </div>
 
-          <div v-if=showAddModal>
+        <Modal v-model="showDeleteModal" title="Delete Product" >
+                    <ConformDelete
+                    :id=id
+                    :data=data
+                    @delete=ConformDelete
+                    />
+        </Modal>
+
+        <Modal v-model="showEditModal" title="Edit Product">
+          <Edit
+                    :categoryList=categoryList
+                    :id=id
+                    :data=data
+                    @edit=Edit
+                    />
+        </Modal>
+         
+        <Modal v-model="showAddModal" title="Add Product">
               <Add
               :categoryList=categoryList
               :data=data
               @add=Add
             />
-          </div>
+ 
+        </Modal>
 
-          <h1 class="mx-6 font-sans block shadow-lg text-5xl font-extrabold text-white rounded-lg border p-1 bg-green-700 my-5" >These are the list of category .</h1> 
-          <div  class=" py-8 px-3  my-5 mx-6 font-sans shadow-2xl w-3/12 bg-white block shadow-xl text-black rounded-lg border-double bg-green-700 text-white">
-            <button @click="toggleCategoryProduct(category)">
-              list of category
-            </button>
-              <div class="flex">
-          
-              </div>
-          </div>    
+        <button class="" @click="toggleAddModal()">
+          <svg class=" w-24 h-24 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/></svg> 
+        </button>         
+
+        <h1 class="mx-6 font-sans block shadow-lg text-5xl font-extrabold text-white rounded-lg border p-1 bg-green-700 my-5" >These are the list of category .</h1> 
+        <div  class=" py-8 px-3  my-5 mx-6 font-sans shadow-2xl w-3/12 bg-white block shadow-xl text-black rounded-lg border-double bg-green-700 text-white">
+          <button @click="toggleCategory(category)">
+            list of category
+          </button>
+        </div>    
             
-          <button>
-              <svg class="w-24 h-24 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/></svg> 
-          </button> 
+        <button>
+            <svg class="w-24 h-24 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/></svg> 
+        </button> 
+
       </div>
   </div>
   <div v-if=showCategoryProduct>
@@ -86,6 +82,9 @@
 </template>
 
 <script>
+import VueModal from '@kouts/vue-modal';
+import '@kouts/vue-modal/dist/vue-modal.css';
+Vue.component('Modal', VueModal);
 import Vue from 'vue'
 import Toaster from 'v-toaster'
 import 'v-toaster/dist/v-toaster.css'
@@ -96,11 +95,11 @@ import Add from '~/components/Add'
 import categoryProduct from './../components/categoryProduct'
 
 export default {
-
   components:{
     ConformDelete,
     Edit,
-    categoryProduct
+    categoryProduct,
+    'Modal': VueModal
     },
   
   data(){
@@ -109,6 +108,7 @@ export default {
       showEditModal : false,
       showAddModal : false,
       showCategoryProduct:false,
+      showModal: false,
       categoryList: [], 
       list : [],
       cpList:[],
@@ -252,7 +252,7 @@ export default {
           console.log(error)
       })},
 
-      toggleCategoryProduct(){
+      toggleCategory(){
         debugger
         this.showCategoryProduct = true
       },
